d3e24f49adc5429a88051b1fd23f725f8d52ccb9
Lakshmi Hasa <lakshmihasa2003@gmail.com>
2026-07-05 18:43:44 +0530

    Vendor parse.py with official evaluator

--- scripts/vendor_official.sh
+++ scripts/vendor_official.sh
@@ -12,9 +12,8 @@ git clone --depth 1 https://github.com/taoyds/test-suite-sql-eval "$TMP/tsse"
 git clone --depth 1 https://github.com/taoyds/spider "$TMP/spider"
 
 echo "Copying files into $DEST ..."
-# test-suite matcher (single-instance exec match)
 cp "$TMP/tsse/exec_eval.py"     "$DEST/exec_eval.py"
-# hardness + SQL parsing (from Spider; test-suite also ships compatible copies)
+cp "$TMP/tsse/parse.py"         "$DEST/parse.py"
 cp "$TMP/spider/evaluation.py"  "$DEST/evaluation.py"
 cp "$TMP/spider/process_sql.py" "$DEST/process_sql.py"
 touch "$DEST/__init__.py"
